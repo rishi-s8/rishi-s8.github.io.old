1f9091777ed0d74364828c7ef91ce3695c2debb9
rishi-s8 <33762894+rishi-s8@users.noreply.github.com>
2018-06-26 14:44:55 +0530

    Fix responsiveness of sidebar: Firefox fix

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
 /*
   Here goes the JAVASCRIPT
 */
+
+$(function() {
+  $('.fa-phone').hover(function() {
+    $('.popup').css('opacity', '1');
+    $('.popup').css('transform', 'scale(1)');
+    $('.popup').css('-webkit-transform', 'scale(1)');
+  }, function() {
+    // on mouseout, reset the background colour
+    $('.popup').css('opacity', '0');
+    $('.popup').css('transform', 'scale(0)');
+    $('.popup').css('-webkit-transform', 'scale(0)');
+  });
+});
